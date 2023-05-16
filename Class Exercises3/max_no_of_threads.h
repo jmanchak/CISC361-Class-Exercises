2dc34f34d472dc37c68494dd47bf7d0866342728
Jessica Manchak <jmanchak@udel.edu>
2023-05-16 13:34:00 -0400

    Final commits for partcipation & class activities

---
+++ Class Exercises3/max_no_of_threads.h
@@ -0,0 +1,13 @@
+//
+//  max_no_of_threads.h
+//  
+//
+//  Created by Jessica Manchak on 5/16/23.
+//
+
+#ifndef max_no_of_threads_h
+#define max_no_of_threads_h
+
+#include <stdio.h>
+
+#endif /* max_no_of_threads_h */
---
+++ Class Exercises3/producer_consumer_problem.h
@@ -0,0 +1,13 @@
+//
+//  producer_consumer_problem.h
+//  
+//
+//  Created by Jessica Manchak on 5/16/23.
+//
+
+#ifndef producer_consumer_problem_h
+#define producer_consumer_problem_h
+
+#include <stdio.h>
+
+#endif /* producer_consumer_problem_h */
